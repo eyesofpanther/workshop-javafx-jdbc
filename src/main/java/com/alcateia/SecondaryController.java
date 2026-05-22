d76ba1df4702090d35b290f5749a737632efe0cd
eyesofpanther <eyesofpanther@gmail.com>
2026-05-22 16:41:04 -0400

    Local project created

---
+++ src/main/java/com/alcateia/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.alcateia;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.alcateia {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+
+    opens com.alcateia to javafx.fxml;
+    exports com.alcateia;
+}
