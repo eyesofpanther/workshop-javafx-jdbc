c9dc5d67e08f4456c8aae5b0813a01106cc2f12c
eyesofpanther <eyesofpanther@gmail.com>
2026-05-22 21:08:59 -0400

    DepartmentList view design

--- src/main/java/com/alcateia/gui/MainViewController.java
+++ src/main/java/com/alcateia/gui/MainViewController.java
@@ -4,15 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.alcateia.Main;
 import com.alcateia.gui.util.Alerts;
-import com.alcateia.App;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
@@ -36,7 +32,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        System.out.println("OnMenuItemSellerAction");
+        loadView("/com/alcateia/gui/DepartmentList.fxml");
     }
 
     @FXML
