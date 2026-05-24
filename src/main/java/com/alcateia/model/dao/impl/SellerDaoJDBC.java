cd0a0e78d52ab0328118d17e5a2d731089a57d2e
eyesofpanther <eyesofpanther@gmail.com>
2026-05-24 09:38:50 -0400

    TextField & DatePicker

--- src/main/java/com/alcateia/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/com/alcateia/model/dao/impl/SellerDaoJDBC.java
@@ -136,7 +136,8 @@ public class SellerDaoJDBC implements SellerDao {
         obj.setName(rs.getString("Name"));
         obj.setEmail(rs.getString("Email"));
         obj.setBaseSalary(rs.getDouble("BaseSalary"));
-        obj.setBirthDate(rs.getDate("BirthDate"));
+//      obj.setBirthDate(rs.getDate("BirthDate")); Formato java.sql.date
+        obj.setBirthDate(new java.util.Date(rs.getTimestamp("BirthDate").getTime()));
         obj.setDepartment(dep);
         return obj;
     }
